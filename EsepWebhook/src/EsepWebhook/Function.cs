36eb21ca1d726499a95ed7a8c20640314c66e752
Alex De Luera <134332668+alexdeluera@users.noreply.github.com>
2025-11-03 14:38:28 -0500

    Fix to clear build errors

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,5 +1,6 @@
+using System.Text;
 using Amazon.Lambda.Core;
-
+using Newtonsoft.Json;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -7,19 +8,17 @@ namespace EsepWebhook;
 
 public class Function
 {
-    
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public string FunctionHandler(object input, ILambdaContext context)
     {
-        //context.Logger.LogInformation($"FunctionHandler received: {input}");
-
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
- 
+
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
 
         var client = new HttpClient();
@@ -27,10 +26,10 @@ public class Function
         {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
         };
-    
+
         var response = client.Send(webRequest);
         using var reader = new StreamReader(response.Content.ReadAsStream());
-            
+
         return reader.ReadToEnd();
     }
 }
--- EsepWebhook/src/EsepWebhook/obj/Debug/net8.0/EsepWebhook.AssemblyInfo.cs
+++ EsepWebhook/src/EsepWebhook/obj/Debug/net8.0/EsepWebhook.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6c9b477610498fa212508a7121950cf9c2a12d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
